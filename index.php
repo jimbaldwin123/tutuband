42065151931ba9d271efca360dd23447fa414c9f
Jim Baldwin <jim@netvendor.net>
2018-10-08 13:41:47 -0700

    test

--- index.php
+++ index.php
@@ -80,7 +80,6 @@ poster gallery
     </div>
 </form>
 </div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 <!--End mc_embed_signup-->
 
 <div align="center"><a href="/video/TUTU BAND_300k.wmv" class="atext">New! Video! Pasadena 1980</a><br /></div>
